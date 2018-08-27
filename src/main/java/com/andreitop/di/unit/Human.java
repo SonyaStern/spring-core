7419e61c108d28648ab4a425c4ea45a2c097f770
SonyaStern <32080819+SonyaStern@users.noreply.github.com>
2018-08-27 23:40:06 +0300

    Update Human.java

--- src/main/java/com/andreitop/di/unit/Human.java
+++ src/main/java/com/andreitop/di/unit/Human.java
@@ -5,14 +5,30 @@ import com.andreitop.di.mount.Mount;
 public class Human implements Unit {
 
     private Mount mount;
+    private String leftHandWeapon;
+    private String rightHandWeapon;
 
-    public Human(Mount mount) {
+    public Human(Mount mount, String leftHandWeapon, String rightHandWeapon) {
         this.mount = mount;
+        this.leftHandWeapon = leftHandWeapon;
+        this.rightHandWeapon = rightHandWeapon;
     }
 
     @Override
     public void mountMove() {
         mount.move();
+    }
+
+    public String getLeftHandWeaponWeapon() {
+        return leftHandWeapon;
+    }
+
+    public String getRightHandWeaponWeapon() {
+        return rightHandWeapon;
+    }
+
+    public Mount getMount() {
+        return mount;
     }
 }
 
